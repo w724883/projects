69aa2fb95f21dca54d9f2d9e4276617d0ea74313
Jerry <806944687@qq.com>
2014-08-19 17:27:15 +0800

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,46 @@
+data = {
+	news:[
+		{"id":"1","src":"http://image.tianjimedia.com/uploadImages/2013/268/N015951P2M17_1000x500.jpg","title":"1照片","comment":{"aa":"照片很漂亮11","bb":"照片很漂亮11"}},
+		{"id":"2","src":"http://image.tianjimedia.com/uploadImages/2013/268/WP8487WI85U0_1000x500.jpg","title":"2照片","comment":{"aa":"漂亮2","bb":"漂亮2"}},
+		{"id":"3","src":"http://g.hiphotos.baidu.com/image/pic/item/0ff41bd5ad6eddc46fc516663bdbb6fd52663308.jpg","title":"3照片","comment":{"bb":"漂亮2"}},
+		{"id":"4","src":"http://h.hiphotos.baidu.com/image/pic/item/bba1cd11728b47103614fe5dc1cec3fdfc032363.jpg","title":"3照片","comment":{"bb":"沙发33"}},
+		{"id":"5","src":"http://a.hiphotos.baidu.com/image/pic/item/3b292df5e0fe99257c818bee36a85edf8cb171fb.jpg","title":"3照片","comment":{"bb":"沙发33"}},
+		{"id":"6","src":"http://d.hiphotos.baidu.com/image/pic/item/1b4c510fd9f9d72a3bbed79ed62a2834359bbb5f.jpg","title":"3照片","comment":{"bb":"沙发33"}},
+		{"id":"7","src":"http://e.hiphotos.baidu.com/image/pic/item/bd315c6034a85edf89ae5cbe4b540923dd547508.jpg","title":"3照片","comment":{"bb":"沙发33"}}
+	],
+	sports:[
+		{"id":"8","src":"http://h.hiphotos.baidu.com/image/pic/item/00e93901213fb80e3c44614634d12f2eb838948d.jpg","title":"4照片","comment":{"bb":"椅子4"}},
+		{"id":"9","src":"http://d.hiphotos.baidu.com/image/pic/item/3bf33a87e950352a958b1afe5143fbf2b3118b59.jpg","title":"3照片","comment":{"aa":"照片很漂亮11","bb":"照片很漂亮11"}},
+		{"id":"10","src":"http://b.hiphotos.baidu.com/image/pic/item/d8f9d72a6059252dad76feae369b033b5bb5b933.jpg","title":"3照片","comment":{"aa":"照片很漂亮11","bb":"照片很漂亮11"}},
+		{"id":"11","src":"http://f.hiphotos.baidu.com/image/pic/item/4a36acaf2edda3ccd9635c5e03e93901203f92cd.jpg","title":"3照片","comment":{"aa":"照片很漂亮11","bb":"照片很漂亮11"}},
+		{"id":"12","src":"http://c.hiphotos.baidu.com/image/pic/item/f636afc379310a5591453d5eb54543a982261032.jpg","title":"3照片","comment":{"aa":"照片很漂亮11","bb":"照片很漂亮11"}},
+		{"id":"13","src":"http://e.hiphotos.baidu.com/image/pic/item/d833c895d143ad4b85cde5a880025aafa50f06f2.jpg","title":"3照片","comment":{"aa":"照片很漂亮11","bb":"照片很漂亮11"}},
+		{"id":"14","src":"http://c.hiphotos.baidu.com/image/pic/item/359b033b5bb5c9ea7f32dd74d739b6003bf3b360.jpg","title":"3照片","comment":{"aa":"照片很漂亮11","bb":"照片很漂亮11"}},
+		{"id":"15","src":"http://ww2.sinaimg.cn/mw690/9d52c073gw1ejco90y81oj20j60pkmza.jpg","title":"5照片","comment":{"bb":"椅子4"}},
+		{"id":"16","src":"http://ww2.sinaimg.cn/mw690/9d52c073gw1ejco92dr3wj20k00qo77y.jpg","title":"5照片","comment":{"bb":"椅子4"}},
+		{"id":"17","src":"http://f.hiphotos.baidu.com/image/pic/item/ac6eddc451da81cb941a9cc65066d01609243138.jpg","title":"5照片","comment":{"bb":"椅子4"}}
+	]
+};
+/*data = {
+	news:[
+		{"src":"../img/1.jpg","title":"1照片","comment":"照片很漂亮11"},
+		{"src":"../img/2.jpg","title":"2照片","comment":"漂亮2"},
+		{"src":"../img/3.jpg","title":"3照片","comment":"沙发33"},
+		{"src":"../img/1 (2).jpg","title":"3照片","comment":"沙发33"},
+		{"src":"../img/2 (2).jpg","title":"3照片","comment":"沙发33"},
+		{"src":"../img/3 (2).jpg","title":"3照片","comment":"沙发33"},
+		{"src":"../img/4 (2).jpg","title":"3照片","comment":"沙发33"}
+	],
+	sports:[
+		{"src":"../img/4.jpg","title":"4照片","comment":"椅子4"},
+		{"src":"../img/5 (2).jpg","title":"3照片","comment":"沙发33"},
+		{"src":"../img/6 (2).jpg","title":"3照片","comment":"沙发33"},
+		{"src":"../img/7.jpg","title":"3照片","comment":"沙发33"},
+		{"src":"../img/6.jpg","title":"3照片","comment":"沙发33"},
+		{"src":"../img/7 (2).jpg","title":"3照片","comment":"沙发33"},
+		{"src":"../img/8.jpg","title":"3照片","comment":"沙发33"},
+		{"src":"../img/5.jpg","title":"5照片","comment":"鞋子55"},
+		{"src":"../img/9.jpg","title":"5照片","comment":"鞋子55"},
+		{"src":"../img/slider.png","title":"5照片","comment":"鞋子55"}
+	]
+};*/
